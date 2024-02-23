1597d9afc7eff04632e53314d43ff17b28775b1a
Akwesi-Bona <arhinbonnah@gmail.com>
2024-02-23 20:37:48 +0000

    error update 3

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,18 +34,14 @@ def login() -> str:
     email = request.form.get('email')
     password = request.form.get('password')
 
-    # Validate the email and password
     if not AUTH.valid_login(email, password):
         abort(401)
 
-    # If the login information is correct, create a new session for the user
     session_id = AUTH.create_session(email)
 
-    # Create a response with a JSON payload
     response = make_response(jsonify({"email": "<user email>",
                              "message": "logged in"}), 200)
 
-    # Store the session ID as a cookie with key "session_id" on the response
     response.set_cookie('session_id', session_id)
 
     return response
@@ -54,59 +50,39 @@ def login() -> str:
 @app.route('/sessions', methods=['DELETE'])
 def logout() -> str:
     """ function to respond to the DELETE /sessions route. """
-    # Get the session ID from the cookies
     session_id = request.cookies.get('session_id')
 
-    # Find the user with the requested session ID
     user = AUTH.get_user_from_session_id(session_id)
 
-    # If the user exists
     if user:
-        # Destroy the session
         AUTH.destroy_session(user.id)
-        # Redirect the user to GET /
         return redirect('/')
     else:
-        # If the user does not exist, respond with a 403 HTTP status
         abort(403)
 
 
 @app.route("/profile", methods=['GET'])
 def profile() -> str:
     """ function that respond to the GET /profile route. """
-    # Get the session ID from the cookies
     session_id = request.cookies.get('session_id')
 
-    # Find the user with the requested session ID
     user = AUTH.get_user_from_session_id(session_id)
 
     # If the user exists
     if user:
-        # Respond with a 200 HTTP status and the user's email
         return jsonify({"email": user.email}), 200
     else:
-        # If the user does not exist, respond with a 403 HTTP status
         abort(403)
 
 
 @app.route("/reset_password", methods=["POST"])
 def get_reset_password_token() -> str:
     """ function that respond to the POST /reset_password route. """
-
-    # Get the email from the form data
     email = request.form.get('email')
-
-    # Try to generate a reset password token for,
-    # the user with the given email
     try:
         reset_token = AUTH.get_reset_password_token(email)
     except ValueError:
-        # If the email is not registered,
-        # abort the request and respond with a 403 status code
         abort(403)
-
-    # If the reset password token is successfully generated,
-    # return a JSON response with the email and reset token
     return jsonify({"email": email, "reset_token": reset_token})
 
 
@@ -114,20 +90,15 @@ def get_reset_password_token() -> str:
 def update_password() -> str:
     """function that respond to the PUT /reset_password route"""
 
-    # Get the email, reset token, and new password from the form data
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
 
     try:
-        # Try to update the password
         AUTH.update_password(reset_token, new_password)
     except ValueError:
-        # If the token is invalid, respond with a 403 HTTP status code
         abort(403)
 
-    # If the token is valid, respond with a 200 HTTP status and,
-    # the email and message
     return jsonify({"email": email, "message": "Password updated"})
 
 
